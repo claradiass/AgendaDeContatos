54c543a5a1c9bc2db33d8c66663928307b721dd1
Eduarda Souto <mariaeduardac604@gmail.com>
2023-11-06 14:47:29 -0300

    .

--- src/main/java/br/edu/ifpb/domain/Contato.java
+++ src/main/java/br/edu/ifpb/domain/Contato.java
@@ -2,6 +2,9 @@ package br.edu.ifpb.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
 
 // Em resumo, a importação import java.io.Serializable; não é usada diretamente no código, mas a implementação da interface Serializable na classe 
 // Contato permite que os objetos dessa classe sejam serializados.
@@ -12,27 +15,28 @@ public class Contato implements Serializable{
     private boolean ligacao;
     private boolean chamadaVideo;
     private String categoria;
-    private String redeSocial;
-    private String contato;
-    private String email;
-
+    private String tipoDeEntrada;
+    private String valorDaEntrada;
+    
 
     // construtor
-    public Contato(String nome,String sobrenome, int contato, boolean chamadaVideo, String categoria){
+    public Contato(String nome,String sobrenome, boolean ligacao, boolean chamadaVideo, String categoria, String tipoDeEntrada, String valorDaEntrada){
         this.nome = nome;
         this.sobrenome = sobrenome;
-        this.contato = contato;
+        this.ligacao = ligacao;
         this.chamadaVideo = chamadaVideo;
         this.categoria = categoria;
+        this.tipoDeEntrada = tipoDeEntrada;
+        this.valorDaEntrada = valorDaEntrada;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Contato contato = (Contato) obj;
-        return this.contato == contato.contato;
-    }
+    //@Override
+    //public boolean equals(Object obj) {
+        //if (this == obj) return true;
+        //if (obj == null || getClass() != obj.getClass()) return false;
+        //Contato contato = (Contato) obj;
+        //return this.contato == contato.contato;
+    //}
 
     // setters e getters
     public String getNome() {
@@ -40,22 +44,6 @@ public class Contato implements Serializable{
     }
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public int getContato() {
-        return contato;
-    }
-
-    public void setContato(int contato) {
-        this.contato = contato;
-    }
-
-    public boolean isChamadaVideo() {
-        return chamadaVideo;
-    }
-
-    public void setChamadaVideo(boolean chamadaVideo) {
-        this.chamadaVideo = chamadaVideo;
     }
 
     public String getSobrenome() {
@@ -66,6 +54,22 @@ public class Contato implements Serializable{
         this.sobrenome = sobrenome;
     }
 
+    public boolean getLigacao(){
+        return ligacao;
+    }
+
+    public void setLigacao(boolean ligacao){
+        this.ligacao = ligacao;
+    }
+
+    public boolean isChamadaVideo() {
+        return chamadaVideo;
+    }
+
+    public void setChamadaVideo(boolean chamadaVideo) {
+        this.chamadaVideo = chamadaVideo;
+    }
+
     public String getCategoria() {
         return categoria;
     }
@@ -74,11 +78,37 @@ public class Contato implements Serializable{
         this.categoria = categoria;
     }
 
+    public String getTipoDeEntrada() {
+        return tipoDeEntrada;
+    }
+
+    public void setTipoDeEntrada(String tipoDeEntrada) {
+        this.tipoDeEntrada = tipoDeEntrada;
+    }
+
+    public String getValorDaEntrada() {
+        return valorDaEntrada;
+    }
+
+    public void setValorDaEntrada(String valorDaEntrada) {
+        this.valorDaEntrada = valorDaEntrada;
+    }
+
+    public void redeSocial(){
+        List<String> aplicativos = new ArrayList<>();
+
+        aplicativos.add("Email");        
+        aplicativos.add("WhatsApp");
+        aplicativos.add("Instagram");
+        aplicativos.add("Telegram");
+
+    }
+
     // sobrescrita
 
     @Override
-    public int hashCode() {
-        return Objects.hash(getContato());
+    public String hashCode() {
+        return Objects.hash(getValorDeEntrada());
     }
 
     // Este método está calculando o código hash (um valor numérico) com base apenas no atributo número 
