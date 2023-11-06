c032125434ce929a24c0c1b71e2a8578b0d0cea5
claradiass <claragoncalves370@gmail.com>
2023-11-06 13:53:52 -0300

    clara

--- src/main/java/br/edu/ifpb/validators/DateValidator.java
+++ src/main/java/br/edu/ifpb/validators/DateValidator.java
@@ -5,25 +5,14 @@ import java.text.SimpleDateFormat;
 
 public class DateValidator implements Validator<String> {
     @Override
-<<<<<<< HEAD
-    public boolean validate(String aniversario) {
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            dateFormat.parse(aniversario);
-=======
     public boolean validate(String data) {
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
             dateFormat.parse(data);
->>>>>>> 0d7b3ba90f0da7e367c305597dfca5a45903112f
 
             return true;
         } catch (ParseException e) {
             return false;
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0d7b3ba90f0da7e367c305597dfca5a45903112f
