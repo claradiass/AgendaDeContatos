1ae15bf6b65adcd2818a97e58db9a633b813d71c
claradiass <claragoncalves370@gmail.com>
2023-11-06 16:54:47 -0300

    oi

--- src/main/java/br/edu/ifpb/AgendaDeContatosConsoleApplication.java
+++ src/main/java/br/edu/ifpb/AgendaDeContatosConsoleApplication.java
@@ -2,42 +2,40 @@ package br.edu.ifpb;
 
 import java.util.*;
 
-import br.edu.ifpb.commands.AdicionarContatoCommand;
-import br.edu.ifpb.commands.BuscarContatoCommand;
 import br.edu.ifpb.commands.CommandExecutor;
 
 
-import br.edu.ifpb.commands.*;
 import br.edu.ifpb.repository.ContatoRepository;
 import br.edu.ifpb.repository.FileDataService;
 
 public class AgendaDeContatosConsoleApplication {
 
         public static void main(String[] args) {
-        ContatoRepository dataService = ContatoRepository.getInstance();
-        dataService.setRepository(new FileDataService());
-        CommandExecutor executor = new CommandExecutor();
+        // ContatoRepository dataService = ContatoRepository.getInstance();
+        // dataService.setRepository(new FileDataService());
+        // CommandExecutor executor = new CommandExecutor();
+        
+        System.out.println("oi");
+        // Scanner sc = new Scanner(System.in);
+        // int opcao = -1;
 
-        Scanner sc = new Scanner(System.in);
-        int opcao = -1;
+        // while (opcao != 7) {
+        //     System.out.println("\n=================================");
+        //     System.out.println("MENU");
+        //     System.out.println("[1] - Adicionar contatos");
+        //     System.out.println("[4] - Buscar contatos");
+        //     System.out.println("[6] - Sair");
 
-        while (opcao != 7) {
-            System.out.println("\n=================================");
-            System.out.println("MENU");
-            System.out.println("[1] - Adicionar contatos");
-            System.out.println("[4] - Buscar contatos");
-            System.out.println("[6] - Sair");
+        //     System.out.print("Digite a opção -> ");
+        //     opcao = sc.nextInt();
+        //     sc.nextLine();
 
-            System.out.print("Digite a opção -> ");
-            opcao = sc.nextInt();
-            sc.nextLine();
-
-            switch (opcao) {
-                case 1 -> executor.executeCommand(new AdicionarContatoCommand());
-                case 4 -> executor.executeCommand(new BuscarContatoCommand());
-                case 6 -> System.out.println("Tchau!");
-                default -> System.out.println("Opção inválida");
-            }
-        }
+        //     switch (opcao) {
+        //         case 1 -> executor.executeCommand(new AdicionarContatoCommand());
+        //         case 4 -> executor.executeCommand(new BuscarContatoCommand());
+        //         case 6 -> System.out.println("Tchau!");
+        //         default -> System.out.println("Opção inválida");
+        //     }
+        // }
     }
 }
